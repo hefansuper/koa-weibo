6f8f877beb2f054b1bfed9ab14d8f4c7dc5d7ae1
stephenHe <hf597833184@163.com>
2024-12-07 17:52:11 +0800

    feat: 更新部分的框架

---
+++ REAMME.md
@@ -0,0 +1,3 @@
+# koa2 weibo
+
+An weibo project used by koa/mysql/redis/sequelize
---
+++ src/db/model/index.js
@@ -0,0 +1,10 @@
+/**
+ * @description 数据模型入口文件
+ * @author 双越老师
+ */
+
+const User = require('./User')
+
+module.exports = {
+    User
+}
