3a3bc46d56444e80f3147717c8e87a93112c3574
stephenHe <hf597833184@163.com>
2024-12-08 22:11:11 +0800

    feat: 数据分层的处理

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -4,43 +4,31 @@
  */
 
 const router = require('koa-router')()
-const { isExist, register, login, deleteCurUser } = require('../../controller/user')
-const userValidate = require('../../validator/user')
-const { genValidator } = require('../../middlewares/validator')
-const { isTest } = require('../../utils/env')
-const { loginCheck } = require('../../middlewares/loginChecks')
 
+ 
+// 构建user的前缀
 router.prefix('/api/user')
 
 // 注册路由
-router.post('/register', genValidator(userValidate), async (ctx, next) => {
+router.post('/register', async (ctx, next) => {
+    // post参数从request.body中获取
     const { userName, password, gender } = ctx.request.body
-    ctx.body = await register({
+    ctx.body = {
         userName,
         password,
-        gender
-    })
+        gender,
+    }
 })
 
-// 用户名是否存在
-router.post('/isExist', async (ctx, next) => {
-    const { userName } = ctx.request.body
-    ctx.body = await isExist(userName)
-})
 
 // 登录
 router.post('/login', async (ctx, next) => {
     const { userName, password } = ctx.request.body
-    ctx.body = await login(ctx, userName, password)
-})
-
-// 删除
-router.post('/delete', loginCheck, async (ctx, next) => {
-    if (isTest) {
-        // 测试环境下，测试账号登录之后，删除自己
-        const { userName } = ctx.session.userInfo
-        ctx.body = await deleteCurUser(userName)
+    ctx.body = {
+        userName,
+        password,
     }
 })
+
 
 module.exports = router
