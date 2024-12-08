0176a96728ea3af800926dcd48027240c4c02b51
stephenHe <hf597833184@163.com>
2024-12-08 19:50:14 +0800

    feat:add desc1

--- src/db/types.js
+++ src/db/types.js
@@ -1,14 +1,17 @@
 /**
  * @description 封装 sequelize 数据类型
- * @author 双越老师
+ * @author stephen
  */
 
 const Sequelize = require('sequelize')
 
 module.exports = {
     STRING: Sequelize.STRING,
+    // 十进制的值 比较小的且明确的情况下使用，区别于int
+    // DECIMAL(10, 2), // 总位数10，小数点后两位
     DECIMAL: Sequelize.DECIMAL,
     TEXT: Sequelize.TEXT,
+    // 整数  如果需要很大的数的时候 可以使用BIGINT
     INTEGER: Sequelize.INTEGER,
-    BOOLEAN: Sequelize.BOOLEAN
+    BOOLEAN: Sequelize.BOOLEAN,
 }
