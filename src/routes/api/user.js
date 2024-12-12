3e5a02fc69540513e9a7c046334f3ec23de4a4df
stephenHe <hf597833184@163.com>
2024-12-13 03:39:41 +0800

    feat: add Validate

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -4,19 +4,25 @@
  */
 
 const router = require('koa-router')()
-
 const { register, isExist } = require('../../controller/user')
+const userValidate = require('../../validator/user')
 
  
 // 构建user的前缀
 router.prefix('/api/user')
 
 // 注册路由
-router.post('/register', async (ctx, next) => {
+router.post(
+    '/register',
+    async (ctx, next) => {
+        await userValidate(ctx.request.body)
+    },
+    async (ctx, next) => {
     // post参数从request.body中获取
-    const { userName, password, gender } = ctx.request.body
-    ctx.body = register({ userName, password, gender })
-})
+        const { userName, password, gender } = ctx.request.body
+        ctx.body = register({ userName, password, gender })
+    }
+)
 
 
 // 登录
