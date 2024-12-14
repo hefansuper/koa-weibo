61dd6eb81768bedd817d9e9ec3ab46aff9b790a1
stephenHe <hf597833184@163.com>
2024-12-14 22:17:01 +0800

    feat: remove unuse

--- test/server.js
+++
@@ -1,9 +0,0 @@
-/**
- * @description jest server
- * @author STEPHEN
- */
-
-const request = require('supertest')
-const server = require('../src/app').callback()
-
-module.exports = request(server)
