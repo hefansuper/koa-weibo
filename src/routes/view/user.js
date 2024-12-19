b170cb44600d5d7bb3df75aa0eea0db9bc4a7335
stephenHe <hf597833184@163.com>
2024-12-19 20:13:14 +0800

    Refresh .gitignore rules

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -1,8 +1,11 @@
-/**
- * @description user view 路由
- *  这个页面路由的目的就是render出对应的前端ejs页面，可以认为就是一个get请求的ssr接口。
+/*
+ * @Author: stephenHe
+ * @Date: 2024-12-04 23:04:19
+ * @LastEditors: stephenHe
+ * @LastEditTime: 2024-12-19 19:55:22
+ * @Description: 这个页面路由的目的就是render出对应的前端ejs页面，可以认为就是一个get请求的ssr接口。
  *  主要的目的就是返回对应的ejs页面。
- * @author stephen
+ * @FilePath: /weibo-koa/src/routes/view/user.js
  */
 
 const router = require('koa-router')()
@@ -36,6 +39,11 @@ router.get('/login', async (ctx, next) => {
 router.get('/register', async (ctx, next) => {
   // 这个render就是render的view/register.ejs
   await ctx.render('register', getLoginInfo(ctx))
+})
+
+router.get('/setting', async (ctx, next) => {
+  // 这个render就是render的view/setting.ejs
+  await ctx.render('setting', ctx.session.userInfo)
 })
 
 module.exports = router
