43387d21035a8b26b5f04aefe464f660f4a3f1cd
stephenHe <hf597833184@163.com>
2024-12-25 17:27:44 +0800

    feat: add profile router

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -2,7 +2,7 @@
  * @Author: stephenHe
  * @Date: 2024-12-24 10:31:21
  * @LastEditors: stephenHe
- * @LastEditTime: 2024-12-24 10:36:54
+ * @LastEditTime: 2024-12-25 17:25:00
  * @Description: blog页面的的路由，render出index.ejs
  * @FilePath: /weibo-koa/src/routes/view/blog.js
  */
@@ -13,6 +13,16 @@ const { loginRedirect } = require('../../middlewares/loginChecks')
 router.get('/', loginRedirect, async (ctx, next) => {
   // 这个render就是render的view/index.ejs
   await ctx.render('index', {})
+})
+
+// 不带用户名的时候就是自己的主页，获取session并调到带用户名的页面。
+// router.get('/profile', loginRedirect, async (ctx, next) => {
+//   const { userName } = ctx.session.userInfo
+//   ctx.redirect(`/profile/${userName}`)
+// })
+
+router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
+  await ctx.render(`profile`, {})
 })
 
 module.exports = router
