7ba95c5a9d79d5c5ab04ff3ed40a29b44967df8e
stephenHe <hf597833184@163.com>
2024-12-27 18:03:02 +0800

    feat: add square cache

--- src/cache/blog.js
+++ src/cache/blog.js
@@ -2,10 +2,14 @@
  * @Author: stephenHe
  * @Date: 2024-12-27 17:32:39
  * @LastEditors: stephenHe
- * @LastEditTime: 2024-12-27 17:39:49
+ * @LastEditTime: 2024-12-27 17:53:01
  * @Description: 博客的缓存文件
  * @FilePath: /weibo-koa/src/cache/blog.js
  */
+
+const { get: redisGet, set: redisSet } = require('./_redis')
+const { getBlogListByUser } = require('../services/blog-profile')
+const { REDIS_SQUARE_KEY_PREFIX } = require('../conf/constant')
 
 /**
  * 从redis中获取对应的博客广场信息，有就获取，没有就设置。
@@ -13,9 +17,19 @@
  * @param {number} pageSize
  */
 const getSquareCacheList = async (pageIndex, pageSize) => {
-  // 0：redis中对应的key的值是：weibo:square:pageIndex_pageSize
-  // 1：有对应的key就从redis中返回
-  // 2：没有对应的key就从数组库中读取并返回，同时设置redis中的key。
+  // 1：redis中对应的key的值是：weibo:square:pageIndex_pageSize
+  const key = `${REDIS_SQUARE_KEY_PREFIX}${pageIndex}_${pageSize}`
+
+  // 2：有对应的key就从redis中返回
+  const redisResult = await redisGet(key)
+  if (redisResult) {
+    return redisResult
+  }
+
+  // 3：没有对应的key就从数组库中读取并返回，同时设置redis中的key,过期时间为60s
+  const dbResult = await getBlogListByUser({ pageIndex, pageSize })
+  redisSet(key, dbResult, 60)
+  return dbResult
 }
 
 module.exports = {
--- src/conf/constant.js
+++ src/conf/constant.js
@@ -2,7 +2,7 @@
  * @Author: stephenHe
  * @Date: 2024-12-04 23:04:19
  * @LastEditors: stephenHe
- * @LastEditTime: 2024-12-25 21:57:44
+ * @LastEditTime: 2024-12-27 17:42:15
  * @Description: 常量集合
  * @FilePath: /weibo-koa/src/conf/constant.js
  */
@@ -10,4 +10,5 @@
 module.exports = {
   PAGE_SIZE: 5,
   DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs',
+  REDIS_SQUARE_KEY_PREFIX: 'weibo:square:',
 }
