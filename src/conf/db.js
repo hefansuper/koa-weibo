000711bd3fafe6e4f4bff7500f469a6ce86d34b7
stephenHe <hf597833184@163.com>
2025-01-06 15:14:26 +0800

    feature: add pm2

--- src/conf/db.js
+++ src/conf/db.js
@@ -1,6 +1,10 @@
-/**
- * @description 存储配置
- * @author stephen
+/*
+ * @Author: stephenHe
+ * @Date: 2024-12-04 20:12:29
+ * @LastEditors: stephenHe
+ * @LastEditTime: 2025-01-06 15:09:33
+ * @Description:存储配置
+ * @FilePath: /weibo-koa/src/conf/db.js
  */
 
 const { isProd } = require('../utils/env')
@@ -29,7 +33,7 @@ if (isProd) {
     // 线上的 mysql 配置
     host: 'localhost',
     user: 'root',
-    password: 'Mysql_2018',
+    password: 'hf123456',
     port: '3306',
     database: 'koa2_weibo_db',
   }
